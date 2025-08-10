919154ca48fa65d6ff54457e83c75ae961a9b638
Max <maksym.mykhailenko@ehealth.gov.ua>
2025-08-11 02:10:45 +0300

    init proj

---
+++ app/comp_store/src/main/java/com/example/comp_store/CompStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.comp_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompStoreApplication.class, args);
+    }
+
+}
