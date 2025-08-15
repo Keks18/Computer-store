a10a920109dc309529bdff0e94ce1fc06ecfce54
Max <maksym.mykhailenko@ehealth.gov.ua>
2025-08-15 20:18:18 +0300

    add liquibase + first migration

---
+++ app/comp_store/src/main/resources/db/changelog/sql/001-create-tables.sql
@@ -0,0 +1,32 @@
+--liquibase formatted sql
+
+--changeset max:001
+CREATE TABLE brand
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+--changeset max:002
+CREATE TABLE cpu
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+--changeset max:003
+DROP TABLE IF EXISTS cpu;
+
+--changeset max:004
+CREATE TABLE category
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+--changeset max:005
+CREATE TABLE product
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
